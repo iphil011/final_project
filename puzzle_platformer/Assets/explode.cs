837ee52a63fba2fb9e7fc8ed09053dd04e771a5d
Ilya Phillips <iphil011@gold.ac.uk>
2018-10-15 11:19:20 +0100

    added pulse explosion and limit

---
+++ puzzle_platformer/Assets/explode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explode : MonoBehaviour {
+    public float radius;
+    public float strength;
+	// Use this for initialization
+	void Start () {
+        radius = 1;
+        strength = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire2")) {
+            Vector2 explosionPos = transform.position;
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos,radius);
+            foreach (Collider2D hit in colliders) {
+                Vector2 vel = hit.transform.position - transform.position;
+                vel.Normalize();
+                Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+                if (rb != null) {
+                    rb.AddForce(vel, ForceMode2D.Impulse);
+                }
+            }
+            Destroy(gameObject);
+        }
+	}
+}
--- puzzle_platformer/Assets/movement.cs
+++ puzzle_platformer/Assets/movement.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 public class movement : MonoBehaviour {
     public float speed;
     public GameObject pulse;
+    public int limit;
+    private int shots;
 	// Use this for initialization
 	void Start () {
-		
+        limit = 3;
+        shots = 0;
 	}
     void FixedUpdate()
     {
         float x = Input.GetAxis("Horizontal") * speed;
         //Vector2 xMove = new Vector2(x, 0);
         transform.Translate(x, 0, 0);
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (Input.GetButtonDown("Fire1")){
-            Instantiate(pulse, new Vector3(mousePos.x,mousePos.y, 0), transform.rotation);
-        }
+        
+        
     }
     // Update is called once per frame
     void Update () {
-		
-	}
+        
+        if (Input.GetButtonDown("Fire1")&&shots<limit)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(pulse, new Vector3(mousePos.x, mousePos.y, 0), transform.rotation);
+            shots++;
+        }
+        if (Input.GetButtonDown("Fire2")) {
+            shots = 0;
+        }
+    }
 }
