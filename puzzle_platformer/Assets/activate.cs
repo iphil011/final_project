2d37d110af886cd29b9b52c009d62cfc03b9cc9d
Ilya Phillips <iphil011@gold.ac.uk>
2018-10-17 16:52:13 +0100

    door and background added

---
+++ puzzle_platformer/Assets/activate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activate : MonoBehaviour {
+    public GameObject trigger;
+    public GameObject door;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == trigger) {
+            door.GetComponent<Collider2D>().enabled = false;
+            door.GetComponent<Renderer>().enabled = false;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == trigger)
+        {
+            door.GetComponent<Collider2D>().enabled = true;
+            door.GetComponent<Renderer>().enabled = true;
+        }
+    }
+}
